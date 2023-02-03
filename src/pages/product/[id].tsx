be3e9335c9f77733ba8a3c2014d7a6e4220c1622
MarcoDevelop <m.miranda2015@outlook.com>
2023-02-03 19:24:28 -0300

    Checkout no Stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -11,10 +11,16 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps ){
+
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId)
+  }
+
   return (
     <ProductContainer>
       <ImageContainer>
@@ -26,7 +32,7 @@ export default function Product({ product }: ProductProps ){
         <span>{product.price}</span>
         <p>{product.description}</p>
       
-        <button>
+        <button onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
@@ -63,12 +69,9 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount! / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hours
   }
-}
-
-function userRouter(): { isFallback: any } {
-  throw new Error('Function not implemented.')
 }
